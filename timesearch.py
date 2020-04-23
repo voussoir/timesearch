a56451108d74a645f7f492271a084987d3e694bc
Ethan Dalool <ethan@voussoir.net>
2020-04-22 23:53:04 -0700

    Add docstring to top of main timesearch.py launch file.

--- timesearch.py
+++ timesearch.py
@@ -1,9 +1,16 @@
+'''
+This is the main launch file for Timesearch.
+
+When you run `python timesearch.py get_submissions -r subredditname` or any
+other command, your arguments will first go to timesearch_modules\__init__.py,
+then into timesearch_modules\get_submissions.py as appropriate for your command.
+'''
+
 import logging
 handler = logging.StreamHandler()
 log_format = '{levelname}:timesearch.{module}.{funcName}: {message}'
 handler.setFormatter(logging.Formatter(log_format, style='{'))
 logging.getLogger().addHandler(handler)
-
 
 import sys
 import timesearch_modules
